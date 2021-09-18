ce20a4dfa29b0217ef7d7a1d091eb120a91e4cca
Jack Bulson <jacktbulson@gmail.com>
2021-09-18 23:55:45 +0100

    Bullet: Stopped trail being deleted
    Bullets now queue_free() after twice the lifetime of the trail particle to ensure that the particles do not disappear while they still have lifetime left.
    
    The bullet is hidden and the collision shape disabled during this time so as not to affect other objects.

--- Weapons/Cannon/Bullet.gd
+++ Weapons/Cannon/Bullet.gd
@@ -8,9 +8,19 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	$Trail.rotation = angular_velocity
-	$Trail.speed_scale = linear_velocity.length() /175
+	#$Trail.speed_scale = linear_velocity.length() /175
 
 func _on_Bullet_body_entered(body: Node) -> void:
-	queue_free()
+	#queue_free()
+	
+	# Stop particle, delete bullet once particle lifetime is up
+	$Trail.emitting = false
+	var time = ($Trail.lifetime * 2) / $Trail.speed_scale
+	get_tree().create_timer(time).connect("timeout", self, "queue_free")
+	
+	# Hide sprite and disable collision so can't be seen while waiting for trail to finish
+	$Sprite.visible = false
+	$CollisionShape2D.disabled = true
+	
 	if body.has_method("take_damage"):
 			body.take_damage(weapon_damage)
