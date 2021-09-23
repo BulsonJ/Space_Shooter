a41e346fff3f540a2eb6065526acf3b1ce1aadae
Jack Bulson <jacktbulson@gmail.com>
2021-09-23 21:56:28 +0100

    Turret Logic Updated
    Turret now get's the closest body, and uses that as the target instead of using the most recent one to enter. This is on a 1s timer so that it is not called continously.

--- Defence/TurretCannon.gd
+++ Defence/TurretCannon.gd
@@ -20,26 +20,40 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	if !_target:
-		return
+	if _target == null:
+		_target = get_closest_target()
 	else:
 		if turret_direct_sight.is_colliding():
 			if turret_direct_sight.get_collider() is Enemy:
 				if weapon_ready:
 					$ShootFX.play()
 					emit_signal("turret_shoot", bullet, $Turret/Muzzle.global_position, Vector2.RIGHT.rotated(turret.global_rotation), 400.0)
-					$Timer.start(1.0)
+					$ShootTimer.start(1.0)
 					weapon_ready = false
 		
-
-	turret.look_at(_target.position)
-	turret.rotation = clamp(turret.rotation, default_rotation - PI / 4, default_rotation + PI / 4)
-	
-func _on_Turret_Vision_body_entered(body : Enemy) -> void:
-	_target = body
+		if $TargetRefreshTimer.is_stopped():
+			$TargetRefreshTimer.start(2)
+			_target = get_closest_target()
+		
+		turret.look_at(_target.position)
+		turret.rotation = clamp(turret.rotation, default_rotation - PI / 4, default_rotation + PI / 4)
 
 func _on_Turret_Vision_body_exited(body: Enemy) -> void:
-	_target = null
+	if _target == body:
+		_target = null
+
+func get_closest_target() -> Enemy:
+	var possibleTargets = $Turret_Vision.get_overlapping_bodies()
+	var closestTarget : Enemy = null
+	if possibleTargets.size() > 0:
+		closestTarget = possibleTargets.front()
+		var closestDistance : float = closestTarget.position.distance_to(position)
+		for target in possibleTargets:
+			# BUG : Not choosing closest, choosing left to right. Maybe global positioning?
+			var targetDistance = target.position.distance_to(position)
+			if closestDistance > targetDistance:
+				closestTarget = target
+	return closestTarget
 
 func _on_Timer_timeout() -> void:
 	weapon_ready = true
@@ -52,3 +66,4 @@ func _set_health(value) -> void:
 	if (health == 0):
 		queue_free()
 		emit_signal("turret_destroyed")
+
